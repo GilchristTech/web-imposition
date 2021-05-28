1a20e029153fe5253054d019122e77f670713e1b
Gilchrist Pitts <me@gilchrist.tech>
2021-05-28 11:36:09 -0700

    Code review with Kirn

--- src/index.js
+++ src/index.js
@@ -6,18 +6,24 @@ function setMargin (margin) {
 
 class ImpositionImage {
   constructor () {
-    this.src;
-    this.element;
-    this.elementSecondary;
+    /*
+      Contains an image and image elements for an imposed page. If the page is
+      full spread, two elements are created for each page in the spread.
+    */
 
-    this.single = null;
+    this.src              = null;  // image URL
+    this.element          = null;  // DOM element containing the image
+    this.elementSecondary = null;  // If the image is a two-page spread, the
+                                   // second half of the image.
+
+    this.is_single_page   = null;
     this.hidden;
   }
 
   onload (e) {
     if (this.element.width > this.element.height) {
       this.element.className = "double";
-      this.single = false;
+      this.is_single_page = false;
 
       const right_img     = this.elementSecondary = document.createElement("img");
       right_img.src       = this.src;
@@ -25,10 +31,11 @@ class ImpositionImage {
     }
     else {
       this.element.className = "single";
-      this.single = true;
+      this.is_single_page = true;
     }
   }
 }
+
 
 class ImpositionBook {
   constructor () {
@@ -46,23 +53,39 @@ class ImpositionBook {
   }
 
   refresh () {
-    this.refreshReal();
-    this.refreshImposed();
+    /*
+      Ensure that the DOM structure for the real page layout and imposed page
+      layout views have all pages in this book, and are in the correct order.
+    */
+
+    this.refreshRealPageOrder();
+    this.refreshImposedPageOrder();
   }
 
-  refreshReal () {
+  refreshRealPageOrder () {
     /*
-      Rebuild the parts of the DOM which contain page layouts, reflowing the
-      booklet page according to the order they are read.
+      Rebuild the parts of the DOM which contain page real-order layouts,
+      reordering the booklet pages according to the order they are read.
     */
 
     this.real_pages.innerHTML = "";
 
-    // Create page spread elements
+    /*
+      Go through each page source, and copy them into the real-order page flow,
+      creating page spread elements
+    */
 
     const spreads = this.spreads = [];
     this.pages = [];
-    let side = 1;
+
+    // Keep track of which side of the spread the current page will be imposed
+    // onto. This is a value of either zero or one, where zero is the first page
+    // which should be read, and 1 is the second. Whether this is the left or
+    // right side of the page is flipped by this.right_to_left, and this
+    // determination can be turned into a string of "left" or "right" by
+    // this.pageSideName(page_side)
+
+    let page_side = 1;
 
     for (let [images_i, image] of Object.entries(this.images)) {
 
@@ -74,36 +97,39 @@ class ImpositionBook {
 
       let spread;
 
-      if (image.single) {
-	if(spreads.length > 1 && side == 1) {
+      if (image.is_single_page) {
+
+	// Either create a new page spread, or use the previous one, depending
+	// on which page_side of the spread we're on.
+
+	if(spreads.length > 1 && page_side == 1) {
 	  spread = spreads[spreads.length-1];
 	}
 	else {
-	  spread = document.createElement("div");
-	  spread.className = "spread";
-	  spread.id = `spread${spreads.length}`;
+	  spread = this.createSpreadElement(`spread${spreads.length}`);
 	  this.real_pages.appendChild(spread);
 	  spreads.push(spread);
 	}
 
-	let page_side = (side ^ this.right_to_left) ? "right" : "left";
+	// Create new page element
 
-	const page = document.createElement("div");
-	page.className = `spread-page spread-${page_side}`;
-	page.id = `page${this.pages.length}`;
+	const page = this.createPageElement(page_side, `page${this.pages.length}`);
 	page.appendChild(image.element);
 	spread.appendChild(page);
 
 	this.pages.push(page);
 
-	side = !side;
+	// The next page will be on the opposide side of a spread
+	page_side = !page_side;
       }
       else {
 	// We're processing a one-image, double-page spread
 
-	spread = document.createElement("div");
-	spread.className = "spread";
+	spread = this.createSpreadElement();
 	this.real_pages.appendChild(spread);
+
+	// Createa page element for each page in the spread, using different
+	// sides of the same image.
 
 	const page_l = document.createElement("div");
 	const page_r = document.createElement("div");
@@ -118,38 +144,47 @@ class ImpositionBook {
 	spread.appendChild(page_r);
 
 	page_l.id = `page${this.pages.length}`;
-	this.pages.push(page_l);
 	page_r.id = `page${this.pages.length}`;
+
+	this.pages.push(page_l);
 	this.pages.push(page_r);
 
-	side = 0;
+	// Because this image takes up a full page spread, start the next page
+	// spread at the first page_side in the reading order.
+	page_side = 0;
       }
     }
 
     /*
-      Append filler pages until the number of pages is divisible by four
+      Append empty filler pages until the number of pages is divisible by four,
+      also taking into account whether the final page image is used as a back
+      cover.
     */
     
+    // Calculate the total number of pages, after filler pages have been added
+
     let postfiller_page_length;
 
     if (this.has_back_cover) {
       postfiller_page_length = (
-	/* # of sheets         */ ((this.pages.length + 3) >> 2)
-	/* # of pages, rounded */ * 4
-	/* reserve back cover  */ - 1
+	((this.pages.length + 3) >> 2)   // # of sheets
+	* 4                              // # of pages, rounded
+	- 1                              // reserve back cover
       );
     }
     else {
       postfiller_page_length = (
-	/* Fill extra pages until */ (
-	/* back cover is blank.   */   (this.pages.length + 4)
-	/* From that, # of sheets */   >> 2
-	/* Round to # of pages    */ ) * 4
+	(                                // Fill extra pages until
+		(this.pages.length + 4)  // back cover is blank.
+		>> 2                     // From that, # of sheets
+	) * 4                            // Round to # of pages
       );
     }
 
+    // Insert blank pages to fill page count
+
     while (this.pages.length < postfiller_page_length) {
-      // If the page is even, then start a new page
+      // If the page number is even, then start a new page
       if (this.pages.length % 2 == 0) {
 	const new_spread     = document.createElement("div");
 	new_spread.className = "spread";
@@ -158,27 +193,30 @@ class ImpositionBook {
       }
 
       const spread           = this.spreads[this.spreads.length - 1];
-      const blank_page       = document.createElement("div");
-      const page_side_number = this.pages.length % 2;
-      const page_side_name   = (page_side_number ^ this.right_to_left) ? "right" : "left";
-      blank_page.className   = `spread-page spread-${page_side_name}`;
-      blank_page.id          = `page${this.pages.length}`;
-      
+
+      const blank_page = this.createPageElement(
+	this.pages.length % 2,
+	`page${this.pages.length}`
+      );
+
       this.pages.push(blank_page);
       spread.appendChild(blank_page);
     }
 
+    // If the back cover is the final page, insert that now
+
     if (this.has_back_cover) {
-      const spread     = document.createElement("div");
-      spread.className = "spread";
-      spread.id        = `spread${this.spreads.length}`;
+      const spread = this.createSpreadElement(
+	`spread${this.spreads.length}`
+      );
+
       this.spreads.push(spread);
       this.real_pages.appendChild(spread);
 
-      const page       = document.createElement("div");
-      const page_side  = (this.right_to_left) ? "right" : "left";
-      page.id          = `pages${this.pages.length}`;
-      page.className   = `spread-page spread-${page_side}`;
+      const page = this.createPageElement(
+	this.right_to_left,
+	`pages${this.pages.length}`
+      );
 
       page.appendChild(this.images[this.images.length - 1].element);
       spread.appendChild(page);
@@ -186,19 +224,19 @@ class ImpositionBook {
     }
   }
 
-  refreshImposed () {
+  refreshImposedPageOrder () {
     this.imposed_pages.innerHTML = "";
 
     let last_sheet = null;
     
-    // Group groups of two spreads each into sheets
+    // Group groups of two spreads each into sheets, by mapping a function over
+    // each spread.
 
     this.mapImposedSpreads( (spread) => {
       let sheet = last_sheet;
 
       if (last_sheet === null) {
-	const new_sheet = document.createElement("div");
-	new_sheet.className = "sheet";
+	const new_sheet = this.createSheetElement();
 	this.imposed_pages.appendChild(new_sheet);
 	sheet = new_sheet;
       }
@@ -210,52 +248,53 @@ class ImpositionBook {
   }
   
   imposedSliceSpreads (start, end) {
+    /*
+      Return an array of page spreads, those from start to end, but reorded into
+      an imposed section.
+    */
+
     const imposed_spreads = Array(this.pages.length >> 1);
 
-    // This imposition algorithm iterates through real-flow (as seen by the
-    // reader) of page spreads, and uses that spread's index for calculation.
+    // This imposition algorithm iterates through the real order (that seen by
+    // the reader) of page spreads, and uses that spread's index for
+    // calculation.
 
     for (
 	let imposed_spread_i = 0 ;
-	imposed_spread_i < this.pages.length / 2 ;
+	imposed_spread_i < (end - start + 1) / 2 ;
 	imposed_spread_i++
     ) {
-      let            left_src_i  = this.pages.length - 1 - imposed_spread_i;
-      let           right_src_i  = imposed_spread_i;
-      
-      // Depending on whether this page spread is on the front or the back of
-      // the sheet, the left and right pages will alternate from taking from the
-      // first or second half of the source page flow. So every other page, we
-      // swap source indexes.
+      // Take one page index from both the front and back of the range of pages
+      // we're imposing. Depending on whether we're on the front or back of this
+      // sheet, these indices should be swapped.
+
+      let  left_src_i  = end - start - 1 - imposed_spread_i;
+      let right_src_i  = imposed_spread_i;
 
       if (imposed_spread_i % 2) {
 	[left_src_i, right_src_i] = [right_src_i, left_src_i];
       }
   
-      const    left_source_spread = this.spreads[left_src_i  >> 1];
-      const   right_source_spread = this.spreads[right_src_i >> 1];
-  
-      const      left_source_side = (left_src_i  % 2)^this.right_to_left ? "left" : "right";
-      const     right_source_side = (right_src_i % 2)^this.right_to_left ? "left" : "right";
-
       const  left_source_element  = this.pages[left_src_i];
       const right_source_element  = this.pages[right_src_i];
  
-      // Create return elements
+      // Create an element for the imposed page spread, to be added to the
+      // return element.
 
-      const imposed_spread_element = document.createElement("div");
-      imposed_spread_element.className = "spread";
+      const imposed_spread_element = this.createSpreadElement();
 
-      let  left_imposed_element, right_imposed_element;
+      // Deep copy page sources
 
       if (left_source_element) {
-	left_imposed_element = left_source_element.cloneNode(true);
-	imposed_spread_element.appendChild(left_imposed_element);
+	imposed_spread_element.appendChild(
+	  left_source_element.cloneNode(true)
+	);
       }
 
       if (right_source_element) {
-	right_imposed_element = right_source_element.cloneNode(true);
-	imposed_spread_element.appendChild(right_imposed_element);
+	imposed_spread_element.appendChild(
+	  right_source_element.cloneNode(true)
+	);
       }
 
       imposed_spreads[imposed_spread_i] = imposed_spread_element;
@@ -266,12 +305,12 @@ class ImpositionBook {
 
   mapImposedSpreads (start, end, func) {
     /*
-      Map a function over this.real_pages.slice(start, end) in imposed order.
-      The start and end arguments are optional, and default to the start and end
-      of this.real_pages.
+      Map a function over a slice in the real_pages array, but in imposed order
+      instead of real order. The start and end arguments are optional, and
+      default to the start and end of this.real_pages
     */
 
-    // Process aruments based on length
+    // Process which arguments have been provided
 
     switch (arguments.length) {
       case 1: [ func, start, end ] = [ start,     0, undefined ]; break;
@@ -284,27 +323,74 @@ class ImpositionBook {
 
     return this.imposedSliceSpreads(start, end).map(func);
   }
+
+  createElement (tag, classes="", id="") {
+    const element     = document.createElement(tag);
+    element.className = classes;
+    element.id        = id;
+    return element;
+  }
+
+  createSheetElement (id="") {
+    return this.createElement("div", "sheet", id);
+  }
+
+  createSpreadElement (id="") {
+    return this.createElement("div", "spread", id);
+  }
+
+  createPageElement (side, id="") {
+    const classes = `spread-page spread-${this.pageSideName(side)}`;
+    return this.createElement("div", classes, id);
+  }
+
+  pageSideName (side_number) {
+    return (side_number ^ this.right_to_left) ? "right" : "left";
+  }
 }
+
 
 const book = new ImpositionBook();
 
+
 function processZip (zipfile) {
+  /*
+    Read a zip file's contents, converting the files into image data URLS, and
+    putting them into ImpositionImage objects. Return a promise that runs once
+    all images are loaded.
+  */
+
   return JSZip.loadAsync(zipfile).then( (zip) => {
 
     const images = book.images = [];
     const promises = [];
+
+    // Go through each file in the zip archive. For each, create a promise, and
+    // return a promise which finishes once each file-read-promise is finished
 
     zip.forEach( (path, file) => {
       const img = new ImpositionImage();
       images.push(img);
       
       const promise = file.async("blob").then( (blob) => {
+
+	// Create a base64 image URI from the file contents, then use that to
+	// create an image element.
+
 	const content = URL.createObjectURL(blob);
 
 	return new Promise( (resolve, reject) => {
 	  img.element = document.createElement("img");
-	  img.element.addEventListener('load', () => { img.onload(); return resolve() });
-	  img.element.addEventListener('error', (error)=> reject(error));
+
+	  // Once the image is loaded, fire the ImpositionImage class' onload
+	  // event
+
+	  img.element.addEventListener('load', () => {
+	    img.onload();
+	    return resolve()
+	  });
+
+	  img.element.addEventListener('error', (error) => reject(error));
 	  img.src = img.element.src = content;
 	});
       });
@@ -408,6 +494,7 @@ window.addEventListener("load", () => {
 
   function handleViewChange (e) {
     document.getElementById("pages-welcome").classList.add("hidden");
+
     if (e.target.id == "select-real-view") {
       book.real_pages.classList.remove('hidden');
       book.imposed_pages.classList.add('hidden');
@@ -418,7 +505,7 @@ window.addEventListener("load", () => {
     }
   }
 
-  document.getElementById("select-real-view").onchange = handleViewChange;
+  document.getElementById("select-real-view").onchange    = handleViewChange;
   document.getElementById("select-imposed-view").onchange = handleViewChange;
 
 });
